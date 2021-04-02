1d3e518c1ae21ee1ed791bbdab58373a94ed1f94
Carly Palicz <carlypalicz@fb.com>
2021-04-02 10:30:51 -0700

    bug fix empty message

--- main.js
+++ main.js
@@ -216,8 +216,7 @@ client.on('message', async (message) => {
             message.delete();
         }
         if (output === ''){
-            console.log("empty otutput");
-            let no_empty = "Sorry" + name + ", I didn't see anything for me to translate! Please follow the !translate command with some English or Stitch. For example, !translate the quick brown fox jumped over the lazy dog"
+            let no_empty = "Sorry " + name + ", I didn't see anything for me to translate! Please follow the !translate command with some English or Stitch. For example, !translate the quick brown fox jumped over the lazy dog"
             message.channel.send(no_empty);
         }
         else if (!praised){
