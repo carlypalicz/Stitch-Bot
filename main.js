a38815674836938bc8aa5f7d5ecaa453b7c5abba
Carly Palicz <carlypalicz@fb.com>
2021-04-02 10:05:33 -0700

    no more april fools

--- main.js
+++ main.js
@@ -108,6 +108,7 @@ client.on('message', async (message) => {
 
         if (args.length === 0){
             let output = "Sorry " + name + ", I didn't see anything for me to translate! Please follow the !translate command with some English or Stitch. For example, !translate the quick brown fox jumped over the lazy dog"
+            console.log("first block");
             message.channel.send(output);
             messsage.delete();
             return;
