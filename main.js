49aa732d515e48a939c76c8298084135b70048a0
Carly Palicz <carlypalicz@fb.com>
2021-04-02 10:13:07 -0700

    bug fix empty message

--- main.js
+++ main.js
@@ -106,7 +106,7 @@ client.on('message', async (message) => {
     console.log("Command executed by: " + name);
     console.log("Message: " + args.join(' '));
 
-        if (args.length === 0){
+        if (args.length === 0 && message.guild != null){
             let no_empty = "Sorry " + name + ", I didn't see anything for me to translate! Please follow the !translate command with some English or Stitch. For example, !translate the quick brown fox jumped over the lazy dog"
             console.log("first block");
             message.channel.send(no_empty);
