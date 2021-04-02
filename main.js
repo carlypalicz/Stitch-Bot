2162a6f50580304fed982e4b28c99598ef3c0db6
Carly Palicz <carlypalicz@fb.com>
2021-04-02 10:19:38 -0700

    bug fix empty message

--- main.js
+++ main.js
@@ -71,6 +71,7 @@ client.on('message', async (message) => {
 
     const args = message.content.slice(prefix.length).split(/ +/); //splits the message into an array
     const command = args.shift().toLowerCase(); //the command to be executed, args is now the message minus the command
+    console.log(message);
 
     if (command === 'introduce'){
         intro = "Hi! I'm Stitch Bot! I can help you translate in and out of Covey's Stitch Cipher. To use this bot, enter one of the following commands:\n\n\t !translate to translate in and out of stitch \n\t !abcs to see the key for Stitch \n\t !ylapples for 11 ylapples :D \n\t !introduce to read this message again \n\nTranslations can be copy/pasted for easy communication throughout the server. Stitch Bot was made by discord user carlycries#3691 - please report any issues with or suggestions for the bot to her, or ask a mod to reach out to her for you! Do not use Stitch Bot as a way to spread any hate or harmful words, I'm meant to be a fun addition to the server and I want to be able to stay!";
