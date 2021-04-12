0f13ddb54f9151ed3958139e04b9be75a40e9547
Carly Palicz <carlypalicz@fb.com>
2021-04-11 22:33:58 -0700

    card sample

--- main.js
+++ main.js
@@ -112,7 +112,7 @@ client.on('message', async (message) => {
             .setTimestamp()
             .setFooter('Some footer text here', 'https://i.imgur.com/wSTFkRM.png');
 
-        channel.send(exampleEmbed);
+        message.channel.send(exampleEmbed);
     }
 
     else if (command === 'translate'){
