eafc41254ec20f877d7bb52eef4056dc6c27f8a8
Carly Palicz <carlypalicz@fb.com>
2021-04-11 22:32:27 -0700

    card sample

--- main.js
+++ main.js
@@ -93,6 +93,28 @@ client.on('message', async (message) => {
         message.channel.send(output);
     }
 
+    else if (command === 'test'){
+        const exampleEmbed = new Discord.MessageEmbed()
+            .setColor('#0099ff')
+            .setTitle('Some title')
+            .setURL('https://discord.js.org/')
+            .setAuthor('Some name', 'https://i.imgur.com/wSTFkRM.png', 'https://discord.js.org')
+            .setDescription('Some description here')
+            .setThumbnail('https://i.imgur.com/wSTFkRM.png')
+            .addFields(
+                { name: 'Regular field title', value: 'Some value here' },
+                { name: '\u200B', value: '\u200B' },
+                { name: 'Inline field title', value: 'Some value here', inline: true },
+                { name: 'Inline field title', value: 'Some value here', inline: true },
+            )
+            .addField('Inline field title', 'Some value here', true)
+            .setImage('https://i.imgur.com/wSTFkRM.png')
+            .setTimestamp()
+            .setFooter('Some footer text here', 'https://i.imgur.com/wSTFkRM.png');
+
+        channel.send(exampleEmbed);
+    }
+
     else if (command === 'translate'){
         let rand = Math.random();
         let z1 = rand < .5;
