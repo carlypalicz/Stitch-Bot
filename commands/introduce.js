8f0e882aca0ff97f39fb74ec9af33f5e880a1943
Carly Palicz <carlypalicz@gmail.com>
2021-06-27 15:26:23 -0700

    whoops fixed introduce command

--- commands/introduce.js
+++ commands/introduce.js
@@ -1,6 +1,6 @@
 module.exports = {
-    name: 'ylapples',
-    description: 'outputs 11 ylapples',
+    name: 'introduce',
+    description: 'introduces himself',
     execute(message, args){
         let intro = "Hi! I'm Stitch Bot! I can help you translate in and out of Covey's Stitch Cipher. To use this bot, enter one of the following commands:\n\n\t !translate to translate in and out of stitch \n\t !abcs to see the key for Stitch \n\t !ylapples for 11 ylapples :D \n\t !introduce to read this message again \n\nTranslations can be copy/pasted for easy communication throughout the server. Stitch Bot was made by discord user carlycries#3691 - please report any issues with or suggestions for the bot to her, or ask a mod to reach out to her for you! Do not use Stitch Bot as a way to spread any hate or harmful words, I'm meant to be a fun addition to the server and I want to be able to stay!";
         message.channel.send(intro);
