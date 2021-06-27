453b7671fd48c7b386af8fa0f0db772b784a79aa
Carly Palicz <carlypalicz@gmail.com>
2021-06-27 01:59:38 -0700

    restructuring pt idk

--- richest.js
+++ richest.js
@@ -3,7 +3,7 @@ const profileModel = require('../models/profileSchema');
 module.exports = {
     name: 'richest',
     description: 'richest command',
-    async execute(message, args){
+    async execute(message, args, name){
         let accs = await profileModel.find({ylapples: {$gte: 0} }, 'userID ylapples').exec();
         console.log(accs);
     }
