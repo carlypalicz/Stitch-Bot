5492d3f951e4176b22891971f63634c2120687f6
Carly Palicz <carlypalicz@gmail.com>
2021-06-26 18:26:49 -0700

    restructuring pt idk

--- main.js
+++ main.js
@@ -99,8 +99,7 @@ client.on('message', async (message) => {
     const command = args.shift().toLowerCase(); //the command to be executed, args is now the message minus the command
 
     if (command === 'introduce'){
-        let intro = "Hi! I'm Stitch Bot! I can help you translate in and out of Covey's Stitch Cipher. To use this bot, enter one of the following commands:\n\n\t !translate to translate in and out of stitch \n\t !abcs to see the key for Stitch \n\t !ylapples for 11 ylapples :D \n\t !introduce to read this message again \n\nTranslations can be copy/pasted for easy communication throughout the server. Stitch Bot was made by discord user carlycries#3691 - please report any issues with or suggestions for the bot to her, or ask a mod to reach out to her for you! Do not use Stitch Bot as a way to spread any hate or harmful words, I'm meant to be a fun addition to the server and I want to be able to stay!";
-        message.channel.send(intro);
+        client.commands.get('introduce').execute(message, args);
     }
 
     else if (command === 'ping'){
