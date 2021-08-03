fdc44ef84c4b6ecd724b89a3cda1825134dd2541
Carly Palicz <carlypalicz@gmail.com>
2021-08-02 23:01:35 -0700

    bot can react to messages

--- commands/react.js
+++ commands/react.js
@@ -1,10 +1,55 @@
+const reactions = new Map([
+    ['🅰️', 'A'],
+    ['🇦', 'A'],
+    ['🅱️', 'B'],
+    ['🇧', 'B'],
+    ['🇨', 'C'],
+    ['🇩', 'D'],
+    ['🇪', 'E'],
+    ['🇫', 'F'],
+    ['🇬', 'G'],
+    ['🇭', 'H'],
+    ['ℹ️', 'I'],
+    ['🇮', 'I'],
+    ['🇯', 'J'],
+    ['🇰', 'K'],
+    ['🇱', 'L'],
+    ['Ⓜ️', 'M'],
+    ['🇲', 'M'],
+    ['🇳', 'N'],
+    ['🅾️', 'O'],
+    ['⭕', 'O'],
+    ['🇴', 'O'],
+    ['🅿️', 'P'],
+    ['🇵', 'P'],
+    ['🇶', 'Q'],
+    ['🇷', 'R'],
+    ['🇸', 'S'],
+    ['🇹', 'T'],
+    ['🇺', 'U'],
+    ['🇻', 'V'],
+    ['🇼', 'W'],
+    ['✖️', 'X'],
+    ['❎', 'X'],
+    ['❌', 'X'],
+    ['🇽', 'X'],
+    ['🇾', 'Y'],
+    ['💤', 'Z'],
+    ['🇿', 'Z'],
+]);
+
 module.exports = {
     name: 'react',
     description: 'add a reaction to a message',
     execute(message){
-        message.channel.send("I will react to this!")
+        message.channel.send("Waiting for you to react!")
         .then(function (msg) {
-            msg.react("👍")
-        })
+            msg.awaitReactions((reaction, user) => user.id == message.author.id,
+                { max: 1, time: 30000 }).then(collected => {
+                    message.channel.send(collected.first().emoji.name);
+                }).catch(() => {
+                    message.channel.send("wahhhh something went wrong");
+                });
+        });
     }
 }
