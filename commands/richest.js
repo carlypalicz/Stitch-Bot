5ee4e59599ba8bc64c9d3ec64e1a5335d9d64c5e
Carly Palicz <carlypalicz@gmail.com>
2021-08-04 16:25:48 -0700

    richest

--- commands/richest.js
+++ commands/richest.js
@@ -5,8 +5,26 @@ module.exports = {
     name: 'richest',
     description: 'richest command',
     async execute(message, args, name){
-        const response = await profileModel.find().sort({ylapples:-1}).limit(10);
-        console.log(response);
+        const richest = await profileModel.find().sort({ylapples:-1}).limit(10);
+
+        const embed = new Discord.MessageEmbed()
+            .setColor('A91B0D')
+            .setTitle('CCS Ylapples Leaderboard')
+            .setDescription(descrip)
+            .addField(richest[0].name, richest[0].ylapples)
+            .addField(richest[1].name, richest[1].ylapples)
+            .addField(richest[2].name, richest[2].ylapples)
+            .addField(richest[3].name, richest[3].ylapples)
+            .addField(richest[4].name, richest[4].ylapples)
+            .addField(richest[5].name, richest[5].ylapples)
+            .addField(richest[6].name, richest[6].ylapples)
+            .addField(richest[7].name, richest[7].ylapples)
+            .addField(richest[8].name, richest[8].ylapples)
+            .addField(richest[9].name, richest[9].ylapples)
+            .setTimestamp();
+        message.channel.send(embed)    
+
+        console.log(richest);
         console.log("richest command executed");
     }
 }
