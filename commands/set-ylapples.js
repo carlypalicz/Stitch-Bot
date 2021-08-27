b98fd5752ab41d510acc29f01189e596bcf88794
Carly Palicz <carlypalicz@gmail.com>
2021-08-26 18:04:16 -0700

    set-ylapples

--- commands/set-ylapples.js
+++ commands/set-ylapples.js
@@ -4,6 +4,10 @@ module.exports = {
     name: 'set-ylapples',
     description: 'set ylapples for users - only carly has permission to do this',
     async execute(message, args, profileData){
+        if (profileData.userID != '427579289102188575'){
+            return message.channel.send("(bonks you on the head) hey !! you do not have permission to use this command.");
+        }
+
         console.log(args);
         const recipient = args[0];
         const amount = args[1];
@@ -16,9 +20,10 @@ module.exports = {
             }, {
                 ylapples: amount
             });
+            return message.channel.send(`Updated the ylapples for ${recipient} to ${amount} ylapples!`);
         } catch(err){
             console.log(err);
+            return message.channel.send('Something went wrong, please ask Carly to check the logs :/');
         }
-        return message.channel.send(`Updated the ylapples for ${recipient} to ${amount} ylapples!`);
     },
 };
