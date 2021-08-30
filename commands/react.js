c879a17e8698015faade3147b5e1bc17369908e0
Carly Palicz <carlypalicz@gmail.com>
2021-08-29 19:10:57 -0700

    hangman progress

--- commands/react.js
+++ commands/react.js
@@ -50,7 +50,7 @@ module.exports = {
         const cooldown_amount =  1000 * 60 * 3; //3 minutes
         const expiration_time = profileData.lastHangman+cooldown_amount;
 
-        if (current_time < profileData.lastDaily+cooldown_amount){
+        if (current_time < profileData.lastHangman+cooldown_amount){
             const time_left = (expiration_time - current_time);
             return message.channel.send(`Please wait ${convert_ms(time_left)} before using this command again.`);
         }
