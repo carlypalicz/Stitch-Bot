4919f1e281c80a533708986f2f79bdcc151186ec
Carly Palicz <carlypalicz@gmail.com>
2021-08-30 13:36:42 -0700

    hangman progress

--- commands/hangman.js
+++ commands/hangman.js
@@ -234,7 +234,7 @@ function loseByWordGuessedWrong(){
     .setTitle('You LOST | Let\'s Play Hangman')
     .setDescription(description)
     .addField('The correct answer was: ', word)
-    .addField('/u200B', '/u200B')
+    .addField('\u200b', '\u200b')
     .addField('Oh No...', orbs(5, false) + cursedFox())
     .setTimestamp();
 }
