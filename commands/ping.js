2764bcc6327ab556dd43d3bb866cca5b861d40cf
Carly Palicz <carlypalicz@gmail.com>
2021-11-13 15:39:58 -0800

    updated discordjs and mongoose

--- commands/ping.js
+++ commands/ping.js
@@ -1,9 +1,6 @@
 module.exports = {
     name: 'ping',
     description: 'ping pong command',
-    init: (client) => {
-        console.log("init has run");
-    },
     execute(message, args){
         message.channel.send('pong!');
     }
