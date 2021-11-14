e1258c5e9b9f0e645bf643d314f9231a3df44cec
Carly Palicz <carlypalicz@gmail.com>
2021-11-13 16:25:02 -0800

    small changes for node update compatibility

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-const {Client, Intents} = require('discord.js');
-const client = new Client({intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES]});
+const {Discord, Intents} = require('discord.js');
+const client = new Discord.Client({intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES]});
 
 client.once('ready', () => {
     console.log('Stitch Bot is online');
