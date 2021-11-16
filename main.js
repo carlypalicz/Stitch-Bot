74ca65a6b7b1f34d7bb54bcc4ea502fdcee9129e
Carly Palicz <carlypalicz@gmail.com>
2021-11-15 17:47:46 -0800

    birthday progress

--- main.js
+++ main.js
@@ -10,7 +10,10 @@ birthdays.set('Danny Davis', {
     zodiac: 'Scorpio',
     bdayBuddy: 'country singer Mason Ramsey',
     message: 'hbd danny!',
-    pronoun: 'he',
+    pronoun1: 'he',
+    pronoun2: 'him',
+    lyric: 'Danny you were always 10 feet from me, laying in your bed',
+    bio: 'Danny is smart and excels in his classes such as math and chemistry, and helped Ray in the creation of fox tears by getting the final ingredient from Tony\'s father (the surgeon). He is inspired by Covey\'s real-life best friend Danny, whom Sound of A Gun is about.',
     color: '947d4c'
 });
 birthdays.set('Bobby and Freddy Freeman', {
@@ -19,7 +22,10 @@ birthdays.set('Bobby and Freddy Freeman', {
     zodiac: 'Scorpio',
     bdayBuddy: 'actor Danny Devito',
     message: 'hbd boby fedy!',
-    pronoun: 'they',
+    pronoun1: 'they',
+    pronoun2: 'them',
+    lyric: 'The fox is looming with intent',
+    bio: 'bio goes here heheheheheh',
     color: 'black'
 });
 // birthdays.set('Leila Lee', {
@@ -167,11 +173,12 @@ client.once('ready', () => {
         const card = new MessageEmbed()
         .setColor(character.color)
         .setTitle(`Happy Birthday ${id}!!!`)
-        .setDescription(`${id} was born on the ${character.day} of ${character.month}. ${character.pronoun} shares a birthday with ${character.bdayBuddy}.`)
+        .setDescription(`${id} was born on the ${character.day} of ${character.month}, making ${character.pronoun2} a ${character.zodiac}. ${character.bio} Fun fact - ${character.pronoun1} shares a birthday with ${character.bdayBuddy}.`)
         .setImage(`https://github.com/carlypalicz/Stitch-Bot/blob/master/students/${linkParam}.png?raw=true`)
+        .setFooter(`*${character.lyric}*`)     
         .setTimestamp(); 
         
-        cron.schedule('21 17 15 November *', () => {
+        cron.schedule('49 17 15 November *', () => {
             client.guilds.cache.get('822718971311685633').channels.cache.get('831039144398028841').send({embeds: [card]});
         },
         {
