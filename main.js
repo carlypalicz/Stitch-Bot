ad617dc89ffe96d231ee257770421365e4fc4773
Carly Palicz <carlypalicz@gmail.com>
2021-11-15 19:57:11 -0800

    birthday progress

--- main.js
+++ main.js
@@ -34,7 +34,7 @@ birthdays.set('Leila Lee', {
     pronouns: she,
     stone: 'topaz',
     holiday: 'National Cake Day and Black Friday. She\'s also quite full from FEASTing for Thanksgiving yesterday, if you catch my drift.',
-    lyric: 'you can eat me, if you need to',
+    lyric: 'You can eat me, if you need to',
     bio: '',
     color: '00adc4',
 
@@ -183,7 +183,7 @@ client.once('ready', () => {
             card = new MessageEmbed()
             .setColor(character.color)
             .setTitle(`Happy Birthday Bobby and Freddy!!!`)
-            .setDescription('Everyone say happy birthday to Bobby and Freddy, who we celebrate today, November 17th. Bobby and Freddy are brothers who were abandoned as children and taken in by the family that found them. They complement each other as near perfect opposites, both in color and personality. Bobby has a beautifully chaotic mind, while Freddy is known for his beautifully structured mind. Bobby and Freddy notably first appeared as paper mache heads on the cover of Some Cats Live, Some Cats Die, and share a last name with Covey themself. Covey has said that they consider Bobby\'s personality to be similar to their own, whereas Freddy\'s personality more mirrors that of drummer Dillon.')
+            .setDescription('Everyone say happy birthday to Bobby and Freddy, who we celebrate today, November 17th. Bobby and Freddy are brothers who were abandoned as children and taken in by the family that found them. They complement each other as near perfect opposites, both in color and personality. They notably first appeared as paper mache heads on the cover of Some Cats Live, Some Cats Die, and share a last name with Covey themself. Covey has said that they consider Bobby\'s personality to be similar to their own, whereas Freddy\'s personality more mirrors that of drummer Dillon.')
             .addField('Boby Fedy Fun Facts', 'Their birthday makes Bobby and Freddy Scorpios, and their birthstone is topaz. They share their birthday today with actor Danny Devito, which is also National Homemade Bread, Baklava, AND Butter Day.')
             .setImage(`https://raw.githubusercontent.com/carlypalicz/Stitch-Bot/master/students/${linkParam}.png`)
             .setFooter(`"${character.lyric}"`);     
@@ -197,7 +197,7 @@ client.once('ready', () => {
             .setFooter(`"${character.lyric}"`);   
         }
         
-        cron.schedule(`53 19 ${character.day} ${character.month} *`, () => {
+        cron.schedule(`59 19 ${character.day} ${character.month} *`, () => {
             client.guilds.cache.get('822718971311685633').channels.cache.get('831039144398028841').send({embeds: [card]});
         },
         {
