c8971f0358ab304d108e0553fed54018bd3f072a
Carly Palicz <carlypalicz@gmail.com>
2021-11-25 16:55:32 -0800

    bday update

--- main.js
+++ main.js
@@ -36,7 +36,7 @@ birthdays.set('Leila Lee', {
     holiday: 'National Cake Day and Black Friday. She\'s also quite full from FEASTing for Thanksgiving yesterday, if you catch my drift.',
     lyric: 'You can eat me, if you need to',
     bio: 'Leila is a loyal friend, and is usually seen with her best friends Martha and Gretta, whom she\'s known since she was 5 and works with at the local ice cream parlor. Her story, along with the story of the songs "You Can Eat Me" and "Plane Crash", are notably inspired by the lateral thinking puzzle known as Albatross Soup.',
-    color: '00adc4',
+    color: '#00adc4',
 });
 birthdays.set('Penny Perkins', {
     month: 'December',
@@ -210,7 +210,7 @@ client.once('ready', () => {
             .setFooter(`"${character.lyric}"`);   
         }
         
-        cron.schedule(`55 16 ${character.day} ${character.month} *`, () => {
+        cron.schedule(`57 16 ${character.day} ${character.month} *`, () => {
             client.guilds.cache.get('822718971311685633').channels.cache.get('831039144398028841').send({embeds: [card]});
         },
         {
