6103d61f0bb6853a0d1897cfda23eaa8e3d3c420
Carly Palicz <carlypalicz@gmail.com>
2022-01-11 18:42:49 -0800

    heroku why

--- commands/scav.js
+++ commands/scav.js
@@ -1,24 +1,4 @@
 const Discord = require('discord.js');
-
-const qmark = '❔';
-
-const steps = new Map();
-steps.set('1', {
-    react: '🦊',
-    text: 'the first puzzle to solve will go here. react with a fox emoji to move to clue 2, or a question mark for a hint',
-    hint: 'u received a clue for puzzle 1',
-})
-steps.set('2', {
-    react: '🐀',
-    text: 'the second puzzle to solve will go here. react with a rat emoji to move to clue 3, or a question mark for a hint',
-    hint: 'u received a clue for puzzle 2',
-})
-steps.set('3', {
-    react: '💀',
-    text: 'the third puzzle to solve will go here. react with a skull emoji to win, or a question mark for a hint',
-    hint: 'u received a clue for puzzle 3',
-})
-
 module.exports = {
     name: 'scav',
     description: 'initiates scavenger hunt',
@@ -28,34 +8,6 @@ module.exports = {
             .setTitle('Happy Birthday Covey!')
             .setDescription('intro to the hunt will go here!')
 
-        message.channel.send({embeds: [embed]})    
-
-        .then(function (msg) {
-            const collector = msg.createReactionCollector(filter);
-
-            collector.on('collect', (reaction, user) => {
-                let emojiname = reaction.emoji.name;
-                console.log(emojiname);
-
-                msg.edit('u replied with an emoji..swag');
-            })
-
-            collector.on('end', collected => {
-                //collector will end when he wins the hunt
-                console.log(`Collected ${collected.size} items`);
-            })
-        });
+        message.channel.send({embeds: [embed]});    
     }
-}
-
-function filter(reaction, user){
-    return (!user.bot);
-}
-
-function nextClue(message){
-    message.reply('ill handle right answers eventually');
-}
-
-function giveHint(message){
-    message.reply('ill give a hint eventually');
 }
