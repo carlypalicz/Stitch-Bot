8a7bcde87eae518b0e16e12101ec30958581cfbc
Carly Palicz <carlypalicz@gmail.com>
2022-01-11 18:02:04 -0800

    'shhhh'

--- commands/scav.js
+++ commands/scav.js
@@ -2,27 +2,31 @@ const Discord = require('discord.js');
 
 const qmark = '❔';
 
-const steps = new Map();
-steps.set('1', {
+let steps = [
+{
     react: '🦊',
     text: 'the first puzzle to solve will go here. react with a fox emoji to move to clue 2, or a question mark for a hint',
     hint: 'u received a clue for puzzle 1',
-})
-steps.set('2', {
+},
+{
     react: '🐀',
     text: 'the second puzzle to solve will go here. react with a rat emoji to move to clue 3, or a question mark for a hint',
     hint: 'u received a clue for puzzle 2',
-})
-steps.set('3', {
+},
+{
     react: '💀',
     text: 'the third puzzle to solve will go here. react with a skull emoji to win, or a question mark for a hint',
     hint: 'u received a clue for puzzle 3',
-})
+}];
+
+let curStep;
+let gameOver;
 
 module.exports = {
     name: 'scav',
     description: 'initiates scavenger hunt',
     execute(message){
+        curStep=0;
         const embed = new Discord.MessageEmbed()
             .setColor('#429196')
             .setTitle('Happy Birthday Covey!')
@@ -35,9 +39,17 @@ module.exports = {
 
             collector.on('collect', (reaction, user) => {
                 let emojiname = reaction.emoji.name;
+                if (emojiname == steps[curStep].react){
+                    nextClue(message);
+                }
+                else if (emojiname == qmark){
+                    giveHint(message);
+                }
+                else {
+                    reaction.remove()
+                    .catch (err => console.log('failed to remove reaction'));
+                }
                 console.log(emojiname);
-
-                msg.edit('u replied with an emoji..swag');
             })
 
             collector.on('end', collected => {
@@ -50,4 +62,12 @@ module.exports = {
 
 function filter(reaction, user){
     return (!user.bot);
+}
+
+function nextClue(message){
+    message.reply('ill handle right answers eventually');
+}
+
+function giveHint(message){
+    message.reply('ill give a hint eventually');
 }
