6bb74fef58a4180d0b1c0d64866d43add1338cbf
Carly Palicz <carlypalicz@gmail.com>
2022-01-11 22:30:18 -0800

    add role command

--- commands/addrole.js
+++ commands/addrole.js
@@ -1,12 +1,12 @@
-const ccid = '427579289102188575';
-const tooth_id = '929132635798794240';
-const yeet = '930708172107296798';
+const ccid = "427579289102188575";
+const tooth_id = "929132635798794240";
+const yeet = "930708172107296798";
 
 module.exports = {
     name: 'addrole',
     description: 'adds tooth role',
     execute(message){
-        let role = message.guild.roles.cache.find(r => r.id === tooth_id);
+        let role = message.guild.roles.cache.find(r => r.id === yeet);
         let member = message.guild.members.cache.get(ccid);
         member.roles.add(role);
     }
