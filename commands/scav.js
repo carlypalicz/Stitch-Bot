3c234e5012bff2c7f63cea9ca611791772d29e85
Carly Palicz <carlypalicz@gmail.com>
2022-01-11 19:07:33 -0800

    map to array

--- commands/scav.js
+++ commands/scav.js
@@ -1,49 +1,55 @@
 const Discord = require('discord.js');
-
 const qmark = '❔';
-
-const steps = new Map();
-steps.set('1', {
+let steps = [
+{
     react: '🦊',
     text: 'the first puzzle to solve will go here. react with a fox emoji to move to clue 2, or a question mark for a hint',
     hint: 'u received a clue for puzzle 1',
-})
-steps.set('2', {
+},
+{
     react: '🐀',
     text: 'the second puzzle to solve will go here. react with a rat emoji to move to clue 3, or a question mark for a hint',
     hint: 'u received a clue for puzzle 2',
-})
-steps.set('3', {
+},
+{
     react: '💀',
     text: 'the third puzzle to solve will go here. react with a skull emoji to win, or a question mark for a hint',
     hint: 'u received a clue for puzzle 3',
-})
-
+}];
+let curStep;
+let gameOver;
 module.exports = {
     name: 'scav',
     description: 'initiates scavenger hunt',
     execute(message){
+        curStep=0;
         const embed = new Discord.MessageEmbed()
             .setColor('#429196')
             .setTitle('Happy Birthday Covey!')
-            .setDescription('intro to the hunt will go here!')
+            .setDescription('intro to the hunt will go here!');
 
         message.channel.send({embeds: [embed]})    
 
         .then(function (msg) {
             const collector = msg.createReactionCollector(filter);
-
             collector.on('collect', (reaction, user) => {
                 let emojiname = reaction.emoji.name;
+                if (emojiname == steps[curStep].react){
+                    nextClue(message);
+                }
+                else if (emojiname == qmark){
+                    giveHint(message);
+                }
+                else {
+                    reaction.remove()
+                    .catch (err => console.log('failed to remove reaction'));
+                }
                 console.log(emojiname);
-
-                msg.edit('u replied with an emoji..swag');
             })
-
             collector.on('end', collected => {
                 //collector will end when he wins the hunt
                 console.log(`Collected ${collected.size} items`);
-            })
+            });
         });
     }
 }
@@ -52,7 +58,7 @@ function filter(reaction, user){
 }
 
 function nextClue(message){
-    message.reply('ill handle right answers eventually');
+    message.reply('ill handle right answers eventually:)');
 }
 
 function giveHint(message){
