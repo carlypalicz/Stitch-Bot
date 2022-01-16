7eeac7831b05cc783fb4e14115246730a7233d56
Carly Palicz <carlypalicz@gmail.com>
2022-01-16 06:52:43 -0800

    small content change

--- main.js
+++ main.js
@@ -258,9 +258,9 @@ client.once('ready', () => {
     let scavInstruc = new MessageEmbed()
         .setColor('#429196')
         .setTitle('🎈 Happy Birthday 🎈!')
-        .addField('Please read below for a surprise! :)', 'Happy birthday Covey!!! Some people in the discord have arranged a small interactive gift in celebration of you!\n\nPlease note that it\'ll take an amount of time + attention to complete, so save it for when you\'re feeling up to it!\n\nOnce ready, simply type "!unwrap" *in this channel* to kick it off.')
+        .addField('Please read below for a surprise! :)', 'Happy birthday Covey!!! Some people in the discord have arranged a small interactive gift in celebration of you!\n\nPlease note that it\'ll take an amount of time + attention to complete, so save it for when you\'re feeling up to it!\n\nOnce ready, simply type "!unwrap" **in this channel** to kick it off.')
         
-    cron.schedule('53 9 16 January *', () => {
+    cron.schedule('54 9 16 January *', () => {
         client.guilds.cache.get(guildID).channels.cache.get(ccsTestChannelID).send({embeds: [scavInstruc]});
     },
     {
